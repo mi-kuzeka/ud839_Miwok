ee7b019016dc432fc3afd50f0208bfdf1026f96e
mi-kuzeka <ckatyao@yahoo.com>
2022-03-06 21:38:17 +0500

    Add ListView and ArrayAdapter for showing numbers in NumbersActivity

--- app/src/main/java/com/example/android/miwok/NumbersActvity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActvity.java
@@ -3,12 +3,28 @@ package com.example.android.miwok;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class NumbersActvity extends AppCompatActivity {
+    ArrayList<String> words;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers_actvity);
-    }
+
+        //Initialize an array of words
+        words = new ArrayList();
+        Collections.addAll(words,
+                "one", "two", "three", "four", "five",
+                "six", "seven", "eight", "nine", "ten");
+
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, words);
+        ListView listView = findViewById(R.id.list);
+        listView.setAdapter(itemsAdapter);
+     }
 }
